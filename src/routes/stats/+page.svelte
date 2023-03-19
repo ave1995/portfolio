f7b9193f55a4a764384620ee64c3ecc699f5c907
ales.vesely <ales.vesely@dataprojekt.cz>
2023-03-19 18:51:22 +0100

    styling table

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -15,13 +15,31 @@
 	</header>
 	<section>
 		<table>
+			<h3>Some stats about me</h3>
 			<TableRow label="Current age" link="" value={$age} />
+		</table>
+		<table>
+			<h3>Some stats about this site</h3>
 			{#await fetchGithubData then Githubdata}
 				{#each Githubdata as data}
-					{@debug data}
 					<TableRow label={data['label']} link={data['link']} value={data['value']} />
 				{/each}
 			{/await}
 		</table>
 	</section>
 </article>
+
+<style>
+	:global(tr td:first-child) {
+		width: 70%;
+	} 
+	:global(td) {
+		border-bottom: 1px solid var(--border-color);
+		padding: 0.75em;
+	}
+	:global(tr:nth-child(even)){background-color: var(--main-color)}
+	table {
+		width: 100%;
+		border-spacing: 0;
+	}
+</style>

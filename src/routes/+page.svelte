aca485b1ec68b34ff027d5031d0d8ba1dbf7bb76
ales.vesely <veselya95@gmail.com>
2023-03-29 19:20:41 +0200

    Source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,14 @@
 	</header>
 	<section>
 		<p>Welcome to my website.</p>
+		<p>
+			Source available
+			<a
+				href="https://github.com/ave1995/portfolio"
+				aria-label="GitHub"
+				target="_blank"
+				rel="noopener noreferrer">here</a
+			>.
+		</p>
 	</section>
 </article>
